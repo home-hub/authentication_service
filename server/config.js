8ea98c51675a7fea551244bdd1b7812f966c5c90
Teddy Andersson <teddy.andersson@infab.nu>
2019-05-05 20:38:05 +0200

    Exporting config variables

---
+++ server/config.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+    port: Number(process.env.PORT),
+    corsOrigin: process.env.CORS_ORIGIN,
+    dbName: process.env.DB_NAME,
+    dbHost: process.env.DB_HOST,
+    dbPort: Number(process.env.DB_PORT) || 27017,
+    jwtSecret: process.env.JWT_SECRET,
+    jwtExpireTime: process.env.JWT_EXPIRE_TIME
+};
